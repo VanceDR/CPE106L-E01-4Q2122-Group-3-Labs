a8b030c845c5072b84dc168614edc8d9501d6362
VanceDelRosario <vancedelrosario@yahoo.com>
2022-06-16 15:37:14 +0800

    deleted for now

--- LR1/PostLab Q1/mean.py
+++
@@ -1,8 +0,0 @@
-def compute_mean(numbers):
-    j = 0
-    computed_mean = 0
-    for i in numbers:
-        computed_mean += i
-        j += 1
-    computed_mean = computed_mean / j
-    return computed_mean
--- LR1/PostLab Q1/mode.py
+++
@@ -1,6 +0,0 @@
-def compute_mode(numbers):
-    n = []
-    for i in numbers:
-        n.append(i)
-    computed_mode = max(n)
-    return computed_mode
